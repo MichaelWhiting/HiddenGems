113ba7ebcbc540d7ea0d6a27c91ddce7cf2ab22a
MichaelWhiting <michaelbwhiting2004@gmail.com>
2024-03-19 12:02:27 -0600

    account page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -8,25 +8,35 @@ function Profile() {
   }
 
   return (
-    <div style={{textAlign: "center"}}>
+    <div style={{textAlign: "center", justifyContent: "center", alignItems: "center", width: "100%", background: "red"}}>
       <div >
         <Icon.Person style={{width: "20%", height: "20%"}}/>
       </div>
-      <h1>{user.email}</h1>
       <h1>{user.username}</h1>
-
+      <h1>{user.email}</h1>
+      <hr/>
       <div style={{textAlign: "center"}}>
+        <h2>Posts</h2>
         <ul>
-          <li>Place 1</li>
-          <li>Place 2</li>
-          <li>Place 3</li>
+          <h4>-Place 1</h4>
+          <h4>-Place 2</h4>
+          <h4>-Place 3</h4>
         </ul>
       </div>
       <div style={{textAlign: "center"}}>
+        <h2>Comments</h2>
         <ul>
-          <li>Friend 1</li>
-          <li>Friend 2</li>
-          <li>Friend 3</li>
+          <h4>-Comment 1</h4>
+          <h4>-Comment 2</h4>
+          <h4>-Comment 3</h4>
+        </ul>
+      </div>
+      <div style={{textAlign: "center"}}>
+        <h2>Friends</h2>
+        <ul>
+          <h4>-Friend 1</h4>
+          <h4>-Friend 2</h4>
+          <h4>-Friend 3</h4>
         </ul>
       </div>
     </div>
