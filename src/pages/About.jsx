3c55bbca6aee9508a8513608ccf5d8250ec78f7f
Jesse Garlick <jessegarlick11@gmail.com>
2024-03-20 11:58:39 -0600

    styling-jesse

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import '../CSS/About.css'; // Assuming you have an About.css file for styling this component
 
 function About() {
   return (
-    <div>
-      About
+    <div className="about-container">
+      <h1 className="about-title">About</h1>
+      <p className="about-content">
+        This is a brief description of what our application does and why it's useful. 
+        You can add more content here to explain your project's goals, 
+        features, or any other information you think might be relevant to users.
+      </p>
     </div>
   )
 }
 
-export default About
+export default About;
