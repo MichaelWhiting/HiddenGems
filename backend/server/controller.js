2fd1c6576e496596af596dfde8572ab127f864b3
MichaelWhiting <michaelbwhiting2004@gmail.com>
2024-03-26 11:08:01 -0600

    got it so you can query and search for friends and see current friends

--- backend/server/controller.js
+++ backend/server/controller.js
@@ -1,4 +1,5 @@
-import { User, Gem, Comment, Rating } from "../database/model.js";
+import { User, Gem, Comment, Rating, Friendship } from "../database/model.js";
+import { Op } from "sequelize";
 
 const handlerFunctions = {
     getUser: async (req, res) => {
@@ -21,8 +22,61 @@ const handlerFunctions = {
     },
 
     getFriends: async (req, res) => {
-        const { userId } = req.params;
-        // add rest of functionality later
+        const userId = req.session.userId;
+
+        const friendships = await Friendship.findAll({
+            where: {
+                userId: userId
+            }
+        });
+
+        const friends = [];
+
+        for (const friendship of friendships) {
+            const user = await User.findByPk(friendship.friendId);
+            friends.push(user);
+        }
+
+        if (friends) {
+            res.send({
+                message: "Retrieved all of the friends for logged in user",
+                success: true,
+                friends
+            });
+            return;
+        } else {
+            res.send({
+                message: "Failed to retrieve friends for logged in user",
+                success: false
+            });
+        }
+    },
+
+    getSearchResults: async (req, res) => {
+        const { searchText } = req.params;
+
+        const searchResults = await User.findAll({
+            where: {
+                email: {
+                    [Op.iLike]: `%${searchText}%`
+                }
+            }
+        });
+
+
+        if (searchResults) {
+            res.send({
+                message: "Found search results for search query",
+                success: true,
+                searchResults
+            });
+            return;
+        } else {
+            res.send({
+                message: "Did not find search results for search query",
+                success: false
+            });
+        }
     },
 
     getGem: async (req, res) => {
--- src/pages/Friends.jsx
+++ src/pages/Friends.jsx
@@ -1,7 +1,8 @@
 import { FloatingLabel, Form, Button } from 'react-bootstrap';
 import * as Icon from "react-bootstrap-icons";
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useState } from 'react';
 import axios from 'axios';
 
 import "../CSS/Friends.css"
@@ -10,30 +11,70 @@ function Friends() {
     const userId = useSelector(state => state.userId);
     const [searchResults, setSearchResults] = useState([]);
     const [currentFriends, setCurrentFriends] = useState([]);
+    const [searchText, setSearchText] = useState("");
+    const navigate = useNavigate();
 
+    useEffect(() =>  {
+        getCurrentFriends();
+    }, [])
+
+    useEffect(() =>  {
+        getSearchResults();
+    }, [searchText])
+    
     const getSearchResults = async () => {
-
+        const { data } = await axios.get(`/getSearchResults/${searchText}`);
+        console.log(data.message);
+        if (data.success) {
+            setSearchResults(data.searchResults);
+            console.log(data.searchResults);
+        }
     }
 
     const getCurrentFriends = async () => {
-
+        const { data } = await axios.get("/getFriends");
+        console.log(data.message);
+        if (data.success) {
+            setCurrentFriends(data.friends);
+            console.log(data.friends);
+        }
     }
-
+    
+    const friendCards = currentFriends.map((friend, i) => {
+        return (
+            <div key={i}>
+                <h3>{friend.email}</h3>
+            </div>
+        )
+    })
+    
     return (
         <div className="friends-container">
             <div className="add-friends">
                 <h1 className="center">Friends</h1>
                 <div className="search-bar">
                     <FloatingLabel label="email" className="custom-outline" style={{width: "60%"}}>
-                        <Form.Control type='text' placeholder="email"/>    
+                        <Form.Control 
+                            type='text' 
+                            placeholder="email"
+                            value={searchText} 
+                            onChange={(e) => {
+                                setSearchText(e.target.value);
+                            }}
+                        />    
                     </FloatingLabel>
-                    <Button variant="info" className="search-button" style={{width: "10%"}}>
+                    <Button 
+                        variant="info" 
+                        className="search-button" 
+                        onClick={getSearchResults}
+                    >
                         <Icon.Search/>
                     </Button>
                 </div>
             </div>
             <div className="current-friends">
                 <h1 className="center">Current Friends:</h1>
+                {friendCards}
             </div>
         </div>
     )
